9ab0e1a783cb4f24c58b37fcf8da2b50f82f88a6
Rizky Ramadhan <ramarizdev@gmail.com>
2024-07-15 01:33:20 +0700

    feat(event): change page size

--- hooks/useEvent.ts
+++ hooks/useEvent.ts
@@ -3,11 +3,17 @@
 import { EventSummary } from '@/types/event';
 import { useQuery } from '@tanstack/react-query';
 import { useSearchParams } from 'next/navigation';
+import qs from 'query-string';
 
 const fetchEvents = async (queryString: string): Promise<EventSummary[]> => {
+  const parsedQuery = qs.parse(queryString);
+  parsedQuery.size = '9';
+  queryString = qs.stringify(parsedQuery);
+
   const response = await fetch(
     `http://localhost:8080/api/v1/events?${queryString}`
   );
+
   const result = await response.json();
   return result.data.content;
 };
